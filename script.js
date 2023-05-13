bb0bbae2c0bf8bd9e6bf2556694d20bea8c68925
Gautham Srinivas <gsri0828@uni.sydney.edu.au>
2023-05-13 19:56:16 +1000

    clicking addSongBtn reveals the input form

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('addSongBtn').addEventListener('click', function() {
+    document.getElementById('songForm').style.display = 'block';
+  });
+  
+  document.getElementById('mySongsNav').addEventListener('click', function() {
+    document.getElementById('songListContainer').style.display = 'block';
+  });
+  

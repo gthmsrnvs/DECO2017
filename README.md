761f82ed91bf5cb83eb745ff2bd7ac6f71ca12d3
Gautham Srinivas <gsri0828@uni.sydney.edu.au>
2023-05-14 22:02:09 +1000

    animate plus button to toggle song form

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # DECO2017 Documentation
 Advanced Web Design Tracker Project
 1. Create a HTML Boilerplate using !
-2. Add a star rating system in the form using a spearate div,
+2. Add a star rating system in the form using radio buttons,
 then make the checkboxes invisible.
--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+//Make the add song form appear when the add song button is clicked
 document.getElementById('addSongBtn').addEventListener('click', function() {
     document.getElementById('songForm').style.display = 'block';
   });
-  
+//make songlist appear when the my songs button is clicked
   document.getElementById('mySongsNav').addEventListener('click', function() {
     document.getElementById('songListContainer').style.display = 'block';
   });

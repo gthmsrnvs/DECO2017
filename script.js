6443fe75e15473cf9d8b17118b84f3e103c2768a
Gautham Srinivas <gsri0828@uni.sydney.edu.au>
2023-05-14 23:17:36 +1000

    made the toggle using .plusButton and spans

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-//Make the add song form appear when the add song button is clicked
-document.getElementById('addSongBtn').addEventListener('click', function() {
-    document.getElementById('songForm').style.display = 'block';
-  });
-//make songlist appear when the my songs button is clicked
-  document.getElementById('mySongsNav').addEventListener('click', function() {
-    document.getElementById('songListContainer').style.display = 'block';
-  });
-  
+const addSongBtn = document.getElementById('addSongBtn');
+const songForm = document.getElementById('songForm');
+const songListContainer = document.getElementById('songListContainer');
+const mySongsNav = document.getElementById('mySongsNav');
+
+// Toggle form display when addSongBtn is clicked
+addSongBtn.addEventListener('click', function() {
+    if(songForm.style.display === 'none' || songForm.style.display === '') {
+        songForm.style.display = 'block';
+        addSongBtn.classList.add('close');
+    } else {
+        songForm.style.display = 'none';
+        addSongBtn.classList.remove('close');
+    }
+});
+
+// Show the song list when My Songs navigation link is clicked
+mySongsNav.addEventListener('click', function() {
+    songListContainer.style.display = 'block';
+});
